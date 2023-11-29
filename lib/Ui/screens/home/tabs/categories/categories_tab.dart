7124f54c8824703c41b15d261977496d60f0e6f3
youssefAnwar87 <yassoyosse@gmail.com>
2023-11-29 17:48:35 +0200

    added assets

--- lib/Ui/screens/home/tabs/categories/categories_tab.dart
+++ lib/Ui/screens/home/tabs/categories/categories_tab.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/Ui/screens/home/tabs/categories/category_widget.dart';
 
 class CategoriesTab extends StatelessWidget {
+  static const String  routeName ="categories";
+
   const CategoriesTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        Text("Pick your category"),
+        GridView.builder(
+          itemCount: 6,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2
+            ),
+            itemBuilder: ( context,index){
+            return CategoryWidget();
+            }
+        )
+      ],
+    );
   }
 }
---
+++ lib/model/category_dm.dart
@@ -0,0 +1,40 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class CategoryDm{
+  String id;
+  String title;
+  String imagePath;
+  bool isLeftSided;
+  Color backgroundColor;
+
+  CategoryDm({required this.imagePath,required this.title,required this.id,
+    required this.backgroundColor,required this.isLeftSided});
+  static List<CategoryDm>categories=[
+    CategoryDm(imagePath: "assets/ball.png",
+        title: "Sports", id: "sports",
+        backgroundColor: Colors.red[900]!, isLeftSided: true),
+
+    CategoryDm(imagePath: "assets/Politics.png",
+        title: "Technology", id: "technology",
+        backgroundColor: Colors.blue[900]!, isLeftSided: false),
+
+    CategoryDm(imagePath: "assets/health.png",
+        title: "Health", id: "health",
+        backgroundColor: Colors.pink, isLeftSided: true),
+
+    CategoryDm(imagePath: "assets/ball.png",
+        title: "Business", id: "business",
+        backgroundColor: Colors.orange[900]!, isLeftSided: false),
+
+    CategoryDm(imagePath: "assets/environment.png",
+        title: "Entertainment", id: "entertainment",
+        backgroundColor: Colors.lightBlue, isLeftSided: true),
+
+    CategoryDm(imagePath: "assets/science.png",
+        title: "Science", id: "science",
+        backgroundColor: Colors.yellow[500]!, isLeftSided: false),
+
+  ];
+}
